6a2ed739da38d34bd05f02ea7046b3f01cc57fa3
Anila <anilayounas41@gmailcom>
2025-07-08 18:34:28 +0500

    Use environment variable for SECRET_KEY

--- backend/sustainx/settings.py
+++ backend/sustainx/settings.py
@@ -1,11 +1,14 @@
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
+
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
